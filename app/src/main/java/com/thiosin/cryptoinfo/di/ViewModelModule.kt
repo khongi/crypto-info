dc2b9113233b840b6fc7d0851cd225ec289a71f0
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-03-12 16:22:11 +0100

    Set up navigation components
    Add navigation view
    Add toolbar

--- app/src/main/java/com/thiosin/cryptoinfo/di/ViewModelModule.kt
+++ app/src/main/java/com/thiosin/cryptoinfo/di/ViewModelModule.kt
@@ -5,18 +5,12 @@ import co.zsmb.rainbowcake.dagger.ViewModelKey
 import com.thiosin.cryptoinfo.ui.about.AboutViewModel
 import com.thiosin.cryptoinfo.ui.details.DetailsViewModel
 import com.thiosin.cryptoinfo.ui.list.ListViewModel
-import com.thiosin.cryptoinfo.ui.pager.PagerViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(PagerViewModel::class)
-    abstract fun bindPagerViewModel(viewModel: PagerViewModel): ViewModel
 
     @Binds
     @IntoMap
--- app/src/main/java/com/thiosin/cryptoinfo/ui/pager/PagerViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.thiosin.cryptoinfo.ui.pager
-
-import co.zsmb.rainbowcake.base.JobViewModel
-import javax.inject.Inject
-
-class PagerViewModel @Inject constructor() : JobViewModel<PagerViewState>(PagerViewState)
