69e3151949b380f97f20f69e63aef9f6d983b3e8
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-03-27 22:16:03 +0100

    Add network calls

--- app/src/main/java/com/thiosin/cryptoinfo/data/network/coinlayer/CoinLayerDataSource.kt
+++ app/src/main/java/com/thiosin/cryptoinfo/data/network/coinlayer/CoinLayerDataSource.kt
@@ -1,5 +1,14 @@
 package com.thiosin.cryptoinfo.data.network.coinlayer
 
+import com.thiosin.cryptoinfo.data.network.coinlayer.models.CoinLayerListApiResponse
+import util.network.NetworkResponse
+import util.network.executeNetworkCall
 import javax.inject.Inject
 
-class CoinLayerDataSource @Inject constructor(private val coinLayerApi: CoinLayerApi)
+class CoinLayerDataSource @Inject constructor(private val coinLayerApi: CoinLayerApi) {
+
+    suspend fun getCoinList(): NetworkResponse<CoinLayerListApiResponse> = executeNetworkCall {
+        coinLayerApi.getList()
+    }
+
+}
--- app/src/main/java/com/thiosin/cryptoinfo/data/network/coinlib/CoinLibDataSource.kt
+++ app/src/main/java/com/thiosin/cryptoinfo/data/network/coinlib/CoinLibDataSource.kt
@@ -1,5 +1,21 @@
 package com.thiosin.cryptoinfo.data.network.coinlib
 
+import com.thiosin.cryptoinfo.data.network.coinlib.models.CoinLibCoinApiResponse
+import com.thiosin.cryptoinfo.data.network.coinlib.models.CoinLibListApiResponse
+import util.network.NetworkResponse
+import util.network.executeNetworkCall
 import javax.inject.Inject
 
-class CoinLibDataSource @Inject constructor(private val coinLibApi: CoinLibApi)
+class CoinLibDataSource @Inject constructor(private val coinLibApi: CoinLibApi) {
+
+    suspend fun getCoin(symbol: String): NetworkResponse<CoinLibCoinApiResponse> =
+        executeNetworkCall {
+            coinLibApi.getCoin(symbol)
+        }
+
+    suspend fun getCoinList(): NetworkResponse<CoinLibListApiResponse> =
+        executeNetworkCall {
+            coinLibApi.getCoinList()
+        }
+
+}
