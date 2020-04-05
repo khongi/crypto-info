bf31101afe2d787181022ed0c390a65c07beacea
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-04-05 18:14:57 +0200

    Fix failing unit test

--- app/src/test/java/com/thiosin/cryptoinfo/domain/interactors/CoinInteractorTest.kt
+++ app/src/test/java/com/thiosin/cryptoinfo/domain/interactors/CoinInteractorTest.kt
@@ -4,6 +4,9 @@ import com.google.common.truth.Truth.assertThat
 import com.thiosin.cryptoinfo.data.disk.DiskDataSource
 import com.thiosin.cryptoinfo.data.network.NetworkDataSource
 import com.thiosin.cryptoinfo.domain.models.DomainCoin
+import com.thiosin.cryptoinfo.domain.models.GetCoinsDto
+import com.thiosin.cryptoinfo.util.network.NetworkResult
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
@@ -12,7 +15,7 @@ import org.junit.Test
 class CoinInteractorTest {
 
     companion object {
-        private val MOCK_COINS = listOf(
+        private val MOCK_DATABASE_COINS = listOf(
             DomainCoin(
                 symbol = "BTC",
                 name = "Bitcoin",
@@ -23,6 +26,16 @@ class CoinInteractorTest {
                 delta7d = 3.3,
                 low24h = 6075.03,
                 high24h = 6267.01,
+                iconUrl = "https://assets.coinlayer.com/icons/BTC.png"
+            )
+        )
+        private val MOCK_NETWORKS_COINS = listOf(
+            GetCoinsDto(
+                symbol = "BTC",
+                name = "Bitcoin",
+                price = 6088.72,
+                rank = 1,
+                delta24h = -2.2,
                 iconUrl = "https://assets.coinlayer.com/icons/BTC.png"
             )
         )
@@ -37,12 +50,13 @@ class CoinInteractorTest {
     )
 
     @Test
-    fun `getCoins returns objects from database`() = runBlocking {
-        every { diskDataSource.getAllCoins() } returns MOCK_COINS
+    fun `getNetworkCoins returns objects from database`() = runBlocking {
+        every { diskDataSource.getAllCoins() } returns MOCK_DATABASE_COINS
+        coEvery { networkDataSource.getCoinList() } returns NetworkResult(MOCK_NETWORKS_COINS)
 
         val response = coinInteractor.getNetworkCoins()
 
-        assertThat(response).contains(MOCK_COINS[0])
+        assertThat(response).contains(MOCK_DATABASE_COINS[0])
     }
 
 }
