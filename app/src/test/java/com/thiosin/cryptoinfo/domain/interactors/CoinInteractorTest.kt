8d2ff2f466d1a694067fcfe1025daa0ece305261
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-04-10 13:04:27 +0200

    Add coin interactor tests
    Refactor getNetworkCoin

--- app/src/test/java/com/thiosin/cryptoinfo/domain/interactors/CoinInteractorTest.kt
+++ app/src/test/java/com/thiosin/cryptoinfo/domain/interactors/CoinInteractorTest.kt
@@ -1,62 +1,149 @@
 package com.thiosin.cryptoinfo.domain.interactors
 
 import com.google.common.truth.Truth.assertThat
+import com.thiosin.cryptoinfo.common.BTC
+import com.thiosin.cryptoinfo.common.DOMAIN_COINS
+import com.thiosin.cryptoinfo.common.DOMAIN_GETCOINDTO
+import com.thiosin.cryptoinfo.common.DOMAIN_GETCOINSDTO_LIST
 import com.thiosin.cryptoinfo.data.disk.DiskDataSource
 import com.thiosin.cryptoinfo.data.network.NetworkDataSource
-import com.thiosin.cryptoinfo.domain.models.DomainCoin
-import com.thiosin.cryptoinfo.domain.models.GetCoinsDto
 import com.thiosin.cryptoinfo.util.network.NetworkResult
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.mockk
+import com.thiosin.cryptoinfo.util.network.NetworkUnavailable
+import io.mockk.*
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
 
 class CoinInteractorTest {
 
-    companion object {
-        private val MOCK_DATABASE_COINS = listOf(
-            DomainCoin(
-                symbol = "BTC",
-                name = "Bitcoin",
-                price = 6088.72,
-                rank = 1,
-                delta1h = -1.1,
-                delta24h = -2.2,
-                delta7d = 3.3,
-                low24h = 6075.03,
-                high24h = 6267.01,
-                iconUrl = "https://assets.coinlayer.com/icons/BTC.png"
-            )
-        )
-        private val MOCK_NETWORKS_COINS = listOf(
-            GetCoinsDto(
-                symbol = "BTC",
-                name = "Bitcoin",
-                price = 6088.72,
-                rank = 1,
-                delta24h = -2.2,
-                iconUrl = "https://assets.coinlayer.com/icons/BTC.png"
-            )
-        )
-    }
-
-    private val networkDataSource: NetworkDataSource = mockk(relaxed = true)
-    private val diskDataSource: DiskDataSource = mockk(relaxed = true)
+    private val networkDataSource: NetworkDataSource = mockk()
+    private val diskDataSource: DiskDataSource = mockk()
 
     private val coinInteractor = CoinInteractor(
         networkDataSource = networkDataSource,
         diskDataSource = diskDataSource
     )
 
+    @Before
+    fun setupUp() {
+        clearAllMocks()
+    }
+
     @Test
-    fun `getNetworkCoins returns objects from database`() = runBlocking {
-        every { diskDataSource.getAllCoins() } returns MOCK_DATABASE_COINS
-        coEvery { networkDataSource.getCoinList() } returns NetworkResult(MOCK_NETWORKS_COINS)
+    fun `getCachedCoins does not access network`() = runBlocking {
+        every { diskDataSource.getAllCoins() } returns DOMAIN_COINS
 
-        val response = coinInteractor.getNetworkCoins()
+        coinInteractor.getCachedCoins()
 
-        assertThat(response).contains(MOCK_DATABASE_COINS[0])
+        coVerify(exactly = 0) { networkDataSource.getCoinList() }
+    }
+
+    @Test
+    fun `getCachedCoins retrieves coins from database`() = runBlocking {
+        every { diskDataSource.getAllCoins() } returns DOMAIN_COINS
+
+        val coins = coinInteractor.getCachedCoins()
+
+        assertThat(coins).isNotNull()
+        assertThat(coins).containsExactlyElementsIn(DOMAIN_COINS).inOrder()
+        verify { diskDataSource.getAllCoins() }
+    }
+
+    @Test
+    fun `getCachedCoins returns null if exception occurs`() = runBlocking {
+        every { diskDataSource.getAllCoins() } throws Throwable()
+
+        val coins = coinInteractor.getCachedCoins()
+
+        assertThat(coins).isNull()
+    }
+
+    @Test
+    fun `getCachedCoinsBySymbol returns empty list if exception occurs`() = runBlocking {
+        every { diskDataSource.getAllCoinsBySymbol(any()) } throws Throwable()
+
+        val coins = coinInteractor.getCachedCoinsBySymbol(BTC.symbol)
+
+        assertThat(coins).isEmpty()
+    }
+
+    @Test
+    fun `getCachedCoinsBySymbol returns correct list`() = runBlocking {
+        every { diskDataSource.getAllCoinsBySymbol(BTC.symbol) } returns listOf(DOMAIN_COINS[0])
+
+        val coins = coinInteractor.getCachedCoinsBySymbol(BTC.symbol)
+
+        assertThat(coins).containsExactly(DOMAIN_COINS[0]).inOrder()
+    }
+
+    @Test
+    fun `getNetworkCoins returns coins from database`() = runBlocking {
+        coEvery { networkDataSource.getCoinList() } returns NetworkResult(DOMAIN_GETCOINSDTO_LIST)
+        every { diskDataSource.updateCoins(any()) } returns Unit
+        every { diskDataSource.getAllCoins() } returns DOMAIN_COINS
+
+        coinInteractor.getNetworkCoins()
+
+        coVerifyOrder {
+            networkDataSource.getCoinList()
+            diskDataSource.updateCoins(any())
+            coinInteractor.getCachedCoins()
+        }
+    }
+
+    @Test
+    fun `getNetworkCoins returns null in case of error`() = runBlocking {
+        coEvery { networkDataSource.getCoinList() } returns NetworkUnavailable
+
+        val coins = coinInteractor.getNetworkCoins()
+
+        assertThat(coins).isNull()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `getCachedCoin throws exception if coin not found`() = runBlocking {
+        every { diskDataSource.getCoinBySymbol(any()) } throws Throwable()
+
+        coinInteractor.getCachedCoin(BTC.symbol)
+        Unit
+    }
+
+    @Test
+    fun `getCachedCoin returns correct coin`() = runBlocking {
+        every { diskDataSource.getCoinBySymbol(BTC.symbol) } returns DOMAIN_COINS[0]
+
+        val coin = coinInteractor.getCachedCoin(BTC.symbol)
+
+        verify { diskDataSource.getCoinBySymbol(BTC.symbol) }
+        assertThat(coin).isEqualTo(DOMAIN_COINS[0])
+    }
+
+    @Test
+    fun `getNetworkCoin updates the coin in database before returning from database`() =
+        runBlocking {
+            coEvery { networkDataSource.getCoin(BTC.symbol) } returns NetworkResult(
+                DOMAIN_GETCOINDTO
+            )
+            every { diskDataSource.updateCoin(any()) } returns Unit
+            every { diskDataSource.getCoinBySymbol(BTC.symbol) } returns DOMAIN_COINS[0]
+
+            val coin = coinInteractor.getNetworkCoin(BTC.symbol)
+
+            coVerifyOrder {
+                networkDataSource.getCoin(BTC.symbol)
+                diskDataSource.updateCoin(any())
+                coinInteractor.getCachedCoin(BTC.symbol)
+            }
+            assertThat(coin).isEqualTo(DOMAIN_COINS[0])
+        }
+
+    @Test
+    fun `getNetworkCoin returns null in case of error`() = runBlocking {
+        coEvery { networkDataSource.getCoin(BTC.symbol) } returns NetworkUnavailable
+
+        val coin = coinInteractor.getNetworkCoin(BTC.symbol)
+
+        assertThat(coin).isNull()
     }
 
 }
